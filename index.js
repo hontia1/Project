671dce0b27a20cddbbcdbdcd07c976716e26e01f
hontia1 <hon_tia1@yahoo.co.uk>
2023-12-04 20:28:04 +0000

    New

---
+++ index.js
@@ -0,0 +1,29 @@
+let text = "My name is Ola"
+console.log(text)
+
+let text1 = "My name is 'Ola'"
+console.log(text1)
+
+let text2 = "My name is \"Ola\" from Nigeria"
+console.log(text2)
+
+let text3= "1234567890"
+console.log(text3.length);
+
+let text4="Banana,Apple,Orange";
+let part = text4.slice(7,12);
+console.log(part);
+
+let singlepart = text4.slice(7);
+console.log(singlepart);
+
+let negpart = text4.slice(-10);
+console.log(negpart);
+
+let negpar = text4.slice(0,-2);
+console.log(negpar);
+
+let negpar1 = text4.slice(0,-12);
+console.log(negpar1);
+
+
